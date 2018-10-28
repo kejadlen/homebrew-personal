70e994212f013a77c0511869bfb94c690afdcb23
Alpha Chen <alpha.chen@gmail.com>
2018-10-27 20:35:48 -0700

    +twitterrific -popcorn-time

---
+++ Casks/twitterrific.rb
@@ -0,0 +1,10 @@
+cask 'twitterrific' do
+  version '5.0'
+  sha256 '18d3a17902048d3804b2b080480acc722c404b8e82872f9537fe3d8d081f42ef'
+
+  url 'https://iconfactory.com/assets/software/phoenix/Twitterrific-5.0+76.zip'
+  name 'Twitterrific'
+  homepage 'https://twitterrific.com/mac'
+
+  app 'Phoenix.app'
+end
